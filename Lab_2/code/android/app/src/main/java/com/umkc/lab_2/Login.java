cc7373c8fb2a9599ec7954f8de9efb07c61784ca
Murali <murali.24.4.91@gmail.com>
2018-10-08 21:18:03 -0500

    Initial design

--- Lab_2/code/android/app/src/main/java/com/umkc/lab_2/Login.java
+++ Lab_2/code/android/app/src/main/java/com/umkc/lab_2/Login.java
@@ -2,6 +2,7 @@ package com.umkc.lab_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
     }
 }
