2cb2ddfc799ce6ce275bd8e83471138daa177d52
Tejaswi789 <42660107+Tejaswi789@users.noreply.github.com>
2018-10-10 23:48:22 -0500

    changes in register page

--- Lab_2/code/ionic/lab2/src/pages/login/login.ts
+++ Lab_2/code/ionic/lab2/src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
 
 /**
  * Generated class for the LoginPage page.
@@ -16,6 +17,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  login()
+  {
+    this.navCtrl.setRoot(RegisterPage);
   }
 
   ionViewDidLoad() {
